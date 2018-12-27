8f315fbcb56c4a172d3c5aebca24486339995435
Chris Lane <chris.lane359@gmail.com>
2018-12-27 04:46:23 -0800

    added IE polyfill

--- app/src/App.js
+++ app/src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
       routingOccurredState: false
     };
     this.routingOccurred = this.routingOccurred.bind(this);
+    this.ie11Polyfill();
   }
 
   routingOccurred(title) {
@@ -36,6 +37,41 @@ class App extends Component {
   componentDidUpdate() {
     if (this.state.routingOccurredState) {
       this.focusTop();
+    }
+  }
+
+  ie11Polyfill() {
+    if (typeof Object.assign != "function") {
+      // Must be writable: true, enumerable: false, configurable: true
+      Object.defineProperty(Object, "assign", {
+        value: function assign(target, varArgs) {
+          // .length of function is 2
+          "use strict";
+          if (target == null) {
+            // TypeError if undefined or null
+            throw new TypeError("Cannot convert undefined or null to object");
+          }
+
+          var to = Object(target);
+
+          for (var index = 1; index < arguments.length; index++) {
+            var nextSource = arguments[index];
+
+            if (nextSource != null) {
+              // Skip over if undefined or null
+              for (var nextKey in nextSource) {
+                // Avoid bugs when hasOwnProperty is shadowed
+                if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                  to[nextKey] = nextSource[nextKey];
+                }
+              }
+            }
+          }
+          return to;
+        },
+        writable: true,
+        configurable: true
+      });
     }
   }
 
